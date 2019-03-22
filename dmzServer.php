4dfbb5eb02cb2f5dc5f4588b954c90223ebb8942
arturo <arturo@arturo-VirtualBox>
2019-03-21 22:32:59 -0400

    add

--- dmzServer.php
+++ dmzServer.php
@@ -15,20 +15,49 @@ function requestProcessor($request)
   switch ($request['feature'])
   {
     case "1":
-      //feature code
-      return $request; //return to front end as array
+	require_once 'unirest-php/src/Unirest.php';
+	$ingredients = $request['ingredient'];
+	$dmzResponse = array();
+	$APIresponse = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?offset=0&query=$ingredients",
+  	array(
+    	"X-RapidAPI-Key" => "f0de423a1fmshf077c4714bd76a2p131d6bjsn0818ef1f78e5"
+ 	)
+	);
+	for ($x = 0; $x <5; $x++)
+	{
+	  $id = $APIresponse->body->results[$x]->id;
+	  $dmzResponse[$x]['id']=$APIresponse->body->results[$x]->id;
+	  $dmzResponse[$x]['tittle']=$APIresponse->body->results[$x]->title;
+	  $dmzResponse[$x]['readyin']=$APIresponse->body->results[$x]->readyInMinutes;
+	  $dmzResponse[$x]['serving']=$APIresponse->body->results[$x]->servings;
+//	  $dmzResponse[$x]['image']=$APIresponse->body->results[$x]->image;
+
+	  $APIresponse2 = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/$id/information",
+  	  array(
+   	  "X-RapidAPI-Key" => "f0de423a1fmshf077c4714bd76a2p131d6bjsn0818ef1f78e5"
+  	  )
+	  );
+	  $dmzResponse[$x]['instructions']=$APIresponse2->body->instructions;
+	}
+	return $dmzResponse;
     case "2":
-      return $request;
+        echo "case 2";
+        break;
     case "3":
-	return $request;
+        echo "case 3";
+        break;
     case "4":
-	return $request;
+        echo "case 4";
+        break;
     case "5":
-      return $request;
+        echo "case 5";
+        break;
     case "6":
-      return $request;
+        echo "case 6";
+        break;
     case "7":
-      return $request;
+        echo "case 7";
+	break;
   }
 }
 
